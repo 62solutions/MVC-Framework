9cbba6a2bfb3eb07bf43aca3d82f06f423dced04
Roman Gulnitskee <ramon@cherryline.ru>
2022-08-02 17:18:51 +0300

    2nd commit

--- app/views/layouts/admin.php
+++ app/views/layouts/admin.php
@@ -28,7 +28,7 @@
     </div>
 
     <?php // debug(vendor\core\Db::$countSql)?>
-    <?php // debug(vendor\core\Db::$queries)?> 
+    <?php // debug(vendor\core\Db::$queries)...?> 
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script> 
